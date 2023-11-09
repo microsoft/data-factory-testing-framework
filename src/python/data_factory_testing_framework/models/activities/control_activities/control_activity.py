314a1f402674dd6106198204f03db2fd281c2842
Arjen Kroezen <arjenkroezen@microsoft.com>
2023-11-09 11:32:34 +0100

    feat: foreach support and refactor model patching

--- src/python/data_factory_testing_framework/models/activities/control_activities/control_activity.py
+++ src/python/data_factory_testing_framework/models/activities/control_activities/control_activity.py
@@ -1,5 +1,6 @@
-from typing import Callable
+from typing import Callable, Generator
 
+from data_factory_testing_framework.generated.models import Activity
 from data_factory_testing_framework.models.state.pipeline_run_state import PipelineRunState
 
 
@@ -9,5 +10,5 @@ class ControlActivity:
     def patch_generated_models(models):
         models.ControlActivity.evaluate_control_activity_iterations = ControlActivity.evaluate_control_activity_iterations
 
-    def evaluate_control_activity_iterations(self, state: PipelineRunState, evaluate: Callable):
+    def evaluate_control_activity_iterations(self, state: PipelineRunState, evaluate_activities: Callable[[PipelineRunState], Generator[Activity, None, None]]):
         return []
