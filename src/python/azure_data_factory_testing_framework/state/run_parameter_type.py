2efa32e38e19a8288bfe2635cb43ab9c17cacb1a
Leonard Herold <92177433+LeonardHd@users.noreply.github.com>
2023-11-17 17:22:45 +0100

    fix: only use parameter.type str representation

--- src/python/azure_data_factory_testing_framework/state/run_parameter_type.py
+++ src/python/azure_data_factory_testing_framework/state/run_parameter_type.py
@@ -8,3 +8,15 @@ class RunParameterType(str, Enum, metaclass=CaseInsensitiveEnumMeta):
     Global = "Global"
     Dataset = "Dataset"
     LinkedService = "LinkedService"
+
+    def __str__(self) -> str:
+        """Get the string representation of the enum.
+
+        We override this method to make sure that the string representation
+        is the same across all Python versions.
+
+        Returns:
+            The string representation of the enum.
+        """
+        super().__str__()
+        return f"{RunParameterType.__name__}.{self.name}"
