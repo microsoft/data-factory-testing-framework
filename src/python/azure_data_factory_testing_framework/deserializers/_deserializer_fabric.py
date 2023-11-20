7afe66ebd408796916f36e26e5c84fead47d81e3
Arjen Kroezen <arjenkroezen@microsoft.com>
2023-11-20 15:15:37 +0100

    test: fix example tests directory

--- src/python/azure_data_factory_testing_framework/deserializers/_deserializer_fabric.py
+++ src/python/azure_data_factory_testing_framework/deserializers/_deserializer_fabric.py
@@ -13,17 +13,15 @@ def parse_pipeline_from_json(json_str: str) -> Pipeline:
     json_data = json.loads(json_str)
 
     properties = json_data.get("properties", {})
-    parameters = properties.get("parameters", {})
-    variables = properties.get("variables", {})
     activities_data = properties.get("activities", [])
-    annotations = properties.get("annotations", [])
 
     activities = []
     for activity_data in activities_data:
         activities.append(_get_activity_from_activity_data(activity_data))
 
     # TODO: Load pipeline_name from item.metadata.json
-    pipeline = Pipeline("batch_job", parameters, variables, activities, annotations)
+    properties["activities"] = activities
+    pipeline = Pipeline("batch_job", **properties)
 
     _find_and_replace_expressions_in_dict(pipeline)
 
