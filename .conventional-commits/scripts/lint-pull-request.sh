6c6010d1ec471abf3d836bcf4fead4088336cd76
Leonard Herold <92177433+LeonardHd@users.noreply.github.com>
2024-05-01 08:54:39 +0200

    ci: check pr title

--- .conventional-commits/scripts/lint-pull-request.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-PR_TITLE="$1"
-COMMIT_LINT_CONFIG="$2"
-
-npm install -g @commitlint/cli@17 @commitlint/config-conventional@17
-
-echo "$PR_TITLE" | commitlint --config "$COMMIT_LINT_CONFIG" --verbose
