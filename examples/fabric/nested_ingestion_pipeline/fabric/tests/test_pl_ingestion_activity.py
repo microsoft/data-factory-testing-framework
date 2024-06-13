cd454bd0644c9fa62adc4989a33ece9d1e3c7a76
Yennifer Santos <46519584+ydaponte@users.noreply.github.com>
2024-06-13 12:58:14 +0200

    docs: add fabric nested pipeline example (#110)
    * docs: add fabric nested pipeline example
    
    * docs: addressing PR comments
    
    * docs: fixing linting
    
    * docs: fix linting
    
    * docs: fix typo
    
    Co-authored-by: Arjen Kroezen <arjenkroezen@microsoft.com>
    
    * docs: fix typo
    
    Co-authored-by: Arjen Kroezen <arjenkroezen@microsoft.com>
    
    * docs: fix typo
    
    Co-authored-by: Arjen Kroezen <arjenkroezen@microsoft.com>
    
    ---------
    
    Co-authored-by: Arjen Kroezen <arjenkroezen@microsoft.com>

---
+++ examples/fabric/nested_ingestion_pipeline/fabric/tests/test_pl_ingestion_activity.py
@@ -0,0 +1,88 @@
+import os
+from pathlib import Path
+
+import pytest
+from data_factory_testing_framework import TestFramework, TestFrameworkType
+from data_factory_testing_framework.models import Pipeline
+from data_factory_testing_framework.state import (
+    PipelineRunState,
+    RunParameter,
+    RunParameterType,
+)
+
+
+@pytest.fixture
+def test_framework(request: pytest.FixtureRequest) -> TestFramework:
+    return TestFramework(
+        framework_type=TestFrameworkType.Fabric,
+        root_folder_path=os.path.join(Path(request.fspath.dirname).parent),
+    )
+
+
+@pytest.fixture
+def pipeline(test_framework: TestFramework) -> Pipeline:
+    return test_framework.get_pipeline_by_name("pl_ingestion")
+
+
+def test_copy_nyc_data_from_web_to_adls2(pipeline: Pipeline) -> None:
+    # Arrange
+    activity = pipeline.get_activity_by_name("Copy NYCData from Web to ADLS")
+    state = PipelineRunState(
+        parameters=[
+            RunParameter(RunParameterType.Pipeline, name="dynamicmonth", value="01"),
+            RunParameter(RunParameterType.Pipeline, name="dynamicyear", value="2023"),
+        ],
+    )
+
+    # Act
+    activity.evaluate(state)
+
+    # Assert
+    assert (
+        activity.type_properties["source"]["datasetSettings"]["typeProperties"]["location"]["relativeUrl"].result
+        == "yellow_tripdata_2023-01.parquet"
+    )
+    assert (
+        activity.type_properties["sink"]["datasetSettings"]["typeProperties"]["location"]["fileName"].result
+        == "yellow_tripdata_2023-01.parquet"
+    )
+    assert (
+        activity.type_properties["sink"]["datasetSettings"]["typeProperties"]["location"]["folderPath"].result
+        == "nyc_taxi_data/2023/01"
+    )
+
+
+def test_copy_nyc_data_from_adls2_to_lakehouse(pipeline: Pipeline) -> None:
+    # Arrange
+    activity = pipeline.get_activity_by_name("Copy NYCData from ADLS to Lakehouse")
+    state = PipelineRunState(
+        parameters=[
+            RunParameter(RunParameterType.Pipeline, name="dynamicmonth", value="01"),
+            RunParameter(RunParameterType.Pipeline, name="dynamicyear", value="2023"),
+        ],
+    )
+
+    # Act
+    activity.evaluate(state)
+
+    # Assert
+    assert (
+        activity.type_properties["source"]["datasetSettings"]["typeProperties"]["location"]["fileSystem"]
+        == "nyctaxidata"
+    )
+    assert (
+        activity.type_properties["source"]["datasetSettings"]["typeProperties"]["location"]["folderPath"].result
+        == "2023/01"
+    )
+    assert (
+        activity.type_properties["source"]["datasetSettings"]["typeProperties"]["location"]["fileName"].result
+        == "yellow_tripdata_2023-01.parquet"
+    )
+    assert (
+        activity.type_properties["sink"]["datasetSettings"]["typeProperties"]["location"]["fileName"].result
+        == "yellow_tripdata_2023-01.parquet"
+    )
+    assert (
+        activity.type_properties["sink"]["datasetSettings"]["typeProperties"]["location"]["folderPath"].result
+        == "nyc_taxi_data/2023/01"
+    )
